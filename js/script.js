e7e01e89b5dad424e82650d5ab958facf78b5606
flavws <persona.fmaria@gmail.com>
2022-05-17 18:03:02 -0300

    scrollspy

--- js/script.js
+++ js/script.js
@@ -67,3 +67,25 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+
+//teste
+
+const section = document.querySelectorAll('section');
+const link = document.querySelectorAll('.nav-link')
+
+window.onscroll = function(){
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 120;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+  
+    if(top>offset && top < offset+height){
+      link.forEach(link => {
+        link.classList.remove('active');
+        document.querySelector('.nav-link[href*='+id+']').classList.add('active')
+      })
+    }
+  })
+}
